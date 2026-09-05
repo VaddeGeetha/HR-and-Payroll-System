cdfc97886e263d9918f5b8e27424654c98c7ebfe
Deepshika3003 <deepshikapagidimarri47@gmail.com>
2026-09-05 21:10:38 +0530

    Update creation(profiles).sql

--- creation(profiles).sql
+++ creation(profiles).sql
@@ -1,6 +1,17 @@
-CREATE TABLE public.profiles (
-    id UUID PRIMARY KEY REFERENCES auth.users(id) ON DELETE CASCADE,
-    full_name TEXT NOT NULL,
-    role TEXT NOT NULL CHECK (role IN ('admin', 'hr', 'employee')),
-    created_at TIMESTAMPTZ DEFAULT NOW()
-);
+create table public.profiles (
+  id uuid not null,
+  full_name text not null,
+  role text not null,
+  created_at timestamp with time zone null default now(),
+  email text null,
+  constraint profiles_pkey primary key (id),
+  constraint profiles_email_key unique (email),
+  constraint profiles_id_fkey foreign KEY (id) references auth.users (id) on delete CASCADE,
+  constraint profiles_role_check check (
+    (
+      role = any (
+        array['admin'::text, 'hr'::text, 'employee'::text]
+      )
+    )
+  )
+) TABLESPACE pg_default;
